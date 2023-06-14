604ac1eb63a7eee7d9c34c6a4cfea36b4be00719
Raymundo Mateos <rayma9829@gmail.com>
2023-06-14 01:12:51 -0600

    [ADD] funcionalidad del getter tokenType

--- 10/sintax_analizer.js
+++ 10/sintax_analizer.js
@@ -1,4 +1,4 @@
-const {readFile, readFileSync} = require('fs');
+const {readFileSync} = require('fs');
 
 const keywords = [
     'class',
@@ -59,26 +59,62 @@ const numberStrings = [
     '9'
 ];  
 
+const tokenTypes = [
+    'KEYWORD',
+    'SYMBOL',
+    'INT_CONST',
+    'STRING_CONST',
+    'IDENTIFIER',
+];
+
+const unicodeRegex = /^"(?:(?![\n"])[\p{L}\p{P}\p{Nd} ])*"$/u;
+const identifierRegex = /(?!.*[" ])(^[A-Za-z_][A-Za-z0-9_]*)/u;
+const numberRegex = /[0-9]+/u;
+
+const  classNames = [];
+
 class JackTokenizer{
     static inputFile;
     static index;
+    static jindex;
     static len;
+    static jlen;
+    static currentLine;
 
     constructor(inputFile = []){
         this.inputFile = inputFile.filter(value => value !== '' || value !== '\n');
         this.index = 0;
+        this.jindex = 0;
         this.len = this.inputFile.length;
     }
 
-    hasMoreTokens(){
-        return this.index < this.len;
+    get hasMoreTokens(){
+        if(this.index == this.len) return false;
+        if(this.inputFile[this.index].includes('"')) this.currentLine = this.inputFile[this.index].trim().split(/ (?=(?:[^"]*"[^"]*")*$)/g);
+        else this.currentLine = this.inputFile[this.index].trim().split(' ');
+        this.jlen = this.currentLine.length;
+        if(this.index == this.len && this.jindex == this.jlen) return false;
+        return true;
     }
 
     advance(){
-        this.index++;
+        if(this.jindex < this.jlen-1){
+            this.jindex++;
+        }
+        else{
+            this.index++;
+            this.jindex = 0;
+        }
     }
 
-    tokenType(){}
+    get tokenType(){
+        const currentToken = this.currentLine[this.jindex];
+        if(keywords.includes(currentToken)) return tokenTypes[0];
+        if(symbols.includes(currentToken)) return tokenTypes[1];
+        if(numberRegex.test(currentToken)) return tokenTypes[2];
+        if(unicodeRegex.test(currentToken)) return tokenTypes[3];
+        if(identifierRegex.test(currentToken)) return tokenTypes[4];
+    }
 
     keyWord(){}
 
@@ -110,9 +146,11 @@ function main(){
     const buffer = readFileSync(filename, {});
     const inputfile = buffer.toString().trim().split('\n');
     const jackTokenizer = new JackTokenizer(inputfile);
-    while(jackTokenizer.hasMoreTokens()){
+    while(jackTokenizer.hasMoreTokens){
+        console.log(jackTokenizer.tokenType);
         jackTokenizer.advance();
     }
 }
 
 main();
+// console.log(identifierRegex.test(' variable'));
