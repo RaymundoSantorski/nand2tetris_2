9ac84a5c96a6f2e5e4cc3505b991a02fb089da36
Raymundo Mateos <rayma9829@gmail.com>
2023-06-16 18:47:02 -0600

    [ADD] funciones keword, symbol, identifier, intVal y steringVal funcionando

--- 10/sintax_analizer.js
+++ 10/sintax_analizer.js
@@ -1,4 +1,4 @@
-const {readFileSync} = require('fs');
+const {readFileSync, openSync, writeFileSync} = require('fs');
 
 const keywords = [
     'class',
@@ -70,6 +70,7 @@ const tokenTypes = [
 const unicodeRegex = /^"(?:(?![\n"])[\p{L}\p{P}\p{Nd} ])*"$/u;
 const identifierRegex = /(?!.*[" ])(^[A-Za-z_][A-Za-z0-9_]*)/u;
 const numberRegex = /[0-9]+/u;
+const commentRegex = /^\/\/[.]*/u;
 
 const  classNames = [];
 
@@ -82,7 +83,8 @@ class JackTokenizer{
     static currentLine;
 
     constructor(inputFile = []){
-        this.inputFile = inputFile.filter(value => value !== '' || value !== '\n');
+        this.inputFile = inputFile.filter(value => (value !== '' || value !== '\n'));
+        this.inputFile = this.inputFile.filter(value => !commentRegex.test(value.trim()));
         this.index = 0;
         this.jindex = 0;
         this.len = this.inputFile.length;
@@ -90,7 +92,7 @@ class JackTokenizer{
 
     get hasMoreTokens(){
         if(this.index == this.len) return false;
-        if(this.inputFile[this.index].includes('"')) this.currentLine = this.inputFile[this.index].trim().split(/ (?=(?:[^"]*"[^"]*")*$)/g);
+        if(this.inputFile[this.index].includes('"')) this.currentLine = this.inputFile[this.index].trim().split(/ (?=(?:[^"]*"[^"]*"|[";])*$)/g);
         else this.currentLine = this.inputFile[this.index].trim().split(' ');
         this.jlen = this.currentLine.length;
         if(this.index == this.len && this.jindex == this.jlen) return false;
@@ -108,7 +110,7 @@ class JackTokenizer{
     }
 
     get _token(){
-        return this.currentLine[this.jindex];
+        return this.currentLine[this.jindex].replace(';', '');
     }
 
     get tokenType(){
@@ -141,6 +143,11 @@ class JackTokenizer{
 }
 
 class CompilationEngine{
+    static outputfile;
+    constructor(outputfile){
+        this.outputfile = outputfile;
+    }
+
     compileExpression(){}
 
     compileTerm(){}
@@ -156,31 +163,48 @@ class CompilationEngine{
 
 function main(){
     const filename = process.argv[2];
-    const buffer = readFileSync(filename, {});
+    const buffer = readFileSync(`${filename}.jack`, {});
+    const outputfile = openSync(`${filename}.xml`, 'w');
     const inputfile = buffer.toString().trim().split('\n');
     const tokenizer = new JackTokenizer(inputfile);
+    const engine = new CompilationEngine(outputfile);
     while(tokenizer.hasMoreTokens){
         const tokenType = tokenizer.tokenType;
+        let tag;
+        let token;
+        console.log(tokenType);
         switch(tokenType){
             case 'KEYWORD':
-                console.log(tokenizer.keyWord);
+                token = tokenizer.keyWord;
+                console.log(`<${tokenType}> ${token} </${tokenType}>\n`);
                 break;
             case 'SYMBOL':
-                console.log(tokenizer.symbol);
+                token = tokenizer.symbol;
+                console.log(`<${tokenType}> ${token} </${tokenType}>\n`);
                 break;
             case 'IDENTIFIER':
-                console.log(tokenizer.identifier);
+                token = tokenizer.identifier;
+                console.log(`<${tokenType}> ${token} </${tokenType}>\n`);
                 break;
             case 'INT_CONST':
-                console.log(tokenizer.intVal);
+                token = tokenizer.intVal;
+                console.log(`<${tokenType}> ${token} </${tokenType}>\n`);
                 break;
             case 'STRING_CONST':
-                console.log(tokenizer.stringVal);
+                token = tokenizer.stringVal;
+                console.log(`<${tokenType}> ${token} </${tokenType}>\n`);
                 break;
+            default:
+                // throw new Error('Instrucción o tipo de dato no reconocido', tokenType);
         }
+        tag = `<${tokenType}> ${token} </${tokenType}>\n`;
+        writeFileSync(outputfile, tag, {
+            encoding: 'utf-8',
+            flag: 'a+',
+        });
         tokenizer.advance();
     }
+    console.log('Hecho!');
 }
 
-main();
-// console.log(identifierRegex.test(' variable'));
+main(); 
